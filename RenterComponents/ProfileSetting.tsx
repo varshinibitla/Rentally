af2339954fb2b794857fae05797cdc15777135f5
Vatsal Navadiya <vnavad01@nyit.edu>
2025-04-22 01:09:34 +1200

    Tidy up

--- RenterComponents/ProfileSetting.tsx
+++ RenterComponents/ProfileSetting.tsx
@@ -5,9 +5,7 @@ const ProfileSettings = ({ navigation }) => {
   return (
     <View style={styles.container}>
         <Text style={styles.switchToText}>Navigate to: </Text>
-        <View style={styles.button}>
-          <Button title="Lander View" onPress={() => navigation.navigate('LenderHome')} />
-        </View>
+        <Button title="Lander Home" onPress={() => navigation.navigate('LenderHome')} />
     </View>
   );
 };
@@ -15,18 +13,15 @@ const ProfileSettings = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'row', // Arrange buttons in a row
+    flexDirection: 'row',
     alignItems: 'flex-start',
     alignContent: 'flex-start',
     flexWrap: 'wrap',
-    marginTop: 20, // Add some margin if needed
+    marginTop: 20,
   },
   switchToText: {
     margin: 20,
     paddingTop: 10,
-  },
-  button: {
-    // margin: 20,
   },
 });
 
